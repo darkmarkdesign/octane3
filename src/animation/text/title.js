a80824e878ca4224b9ecd58c585b9a62dbff89ac
darkmarkdesign <115876728+darkmarkdesign@users.noreply.github.com>
2024-05-06 03:52:06 -0700

    Initial commit

---
+++ src/animation/text/title.js
@@ -0,0 +1,27 @@
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+export default class SlideUp {
+	constructor() {
+		this.el = document.querySelectorAll('[data-animation="slide-up"]');
+
+		this.animate();
+	}
+
+	animate() {
+		this.el.forEach((el) => {
+			gsap.from(el, {
+				scrollTrigger: {
+					trigger: el,
+					start: 'top 97%',
+				},
+				yPercent: 150,
+				duration: 1.15,
+				stagger: { each: 0.05 },
+				ease: 'power3.out',
+			});
+			// ScrollTrigger.refresh();
+		});
+	}
+}
